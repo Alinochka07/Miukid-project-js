e173aaed4996f0bfd65492bf275b8ea8663f0572
Alinochka07 <alina011988@icloud.com>
2023-05-29 15:13:37 +0600

    added: add to cart

---
+++ script/modules/add_to_card.js
@@ -0,0 +1,91 @@
+(function(){
+        "use strict";
+
+// Добавление товара в корзину
+
+function add_to_card(funct_delete_items_from_cart, calculate_cart, count_items_in_cart) {
+     
+        function addToCartFunction(img, ttl, curr, prc, event) {
+                event.preventDefault();
+                let image = img;
+                let title = ttl;
+                let currency = curr;
+                let price = prc;
+                
+        
+                let create_li = document.createElement('li');
+                create_li.classList.add("bag_item");
+        
+        
+                create_li.innerHTML = '<div class="row">\
+                                                <div class="col-sm-4">\
+                                                                <div class="cart_img">\
+                                                                        <img src="'+image[0].src+'" class="shopping_cart_img">\
+                                                                </div>\
+                                                </div>\
+                                                <div class="col-sm-8 left-side">\
+                                                                <div class="cart_view">\
+                                                                        <div><a href=""><p>'+title[0].innerText+'</p></a></div>\
+                                                                        <div class="price">\
+                                                                                <span class="currency">'+currency[0].innerText+'</span>\
+                                                                                <span class="n_price">'+price[0].innerText+'</span>\
+                                                                        </div>\
+                                                                        <div class="cart_trash"><a href=""><i class="far fa-trash-alt"></i></a></div>\
+                                                                </div>\
+                                                </div>\
+                                        </div>';
+        
+        
+                let block_cart = document.querySelector(".cart_submenu .item .bag_items");
+                block_cart.appendChild(create_li);
+                // alert("This product has been successfully added to your cart!");
+                event.preventDefault();
+        
+                       
+                
+                // Вызов функции по удалению товара 
+                funct_delete_items_from_cart(calculate_cart, count_items_in_cart);
+                
+                //  Вызов функции по подсчету общей суммы в корзине
+                calculate_cart();
+        
+                // Подсчет количества товаров в корзине
+                count_items_in_cart();
+        
+        
+                fetch("http://localhost:3000/requests", {
+                        method: "POST",
+                        headers: {
+                                "Content-type": "Application/json"
+                        },
+                        body: JSON.stringify({
+                                "image": image[0].src,
+                                "title": title[0].innerText,
+                                "currency": currency[0].innerText,
+                                "price": price[0].innerText
+                        })
+                });
+        
+        };
+
+        document.querySelector(".product_list").addEventListener("click", event => {
+                event.preventDefault();
+
+                if(event.target.parentNode.classList.contains("product")) return false;
+
+                var img = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".image img");
+                var ttl = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".title");
+                var curr = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".currency");
+                var prc = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".n_price");
+                
+                addToCartFunction(img, ttl, curr, prc);
+
+        });
+};
+
+
+module.exports = add_to_card;
+
+
+
+}());
--- script/script.js
+++ script/script.js
@@ -3,8 +3,12 @@ import $ from "jquery";
 
 (function() {
     const search_dropdown = require("./modules/search_dropdown");
+    get_product_to_cart = require("./module/get_product_to_cart.js"),
+    add_to_card = require("./module/add_to_card.js");
 
-    search_dropdown()
+    get_product_to_cart();
+    search_dropdown();
+    add_to_card();
 
 
 
