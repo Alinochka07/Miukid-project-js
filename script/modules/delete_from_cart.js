d828c275076335f987769c924fb78525e0d9ab2e
Alinochka07 <alina011988@icloud.com>
2023-05-29 15:16:02 +0600

    added: delete from cart

---
+++ script/modules/delete_from_cart.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// Deletion products from cart
+
+function funct_delete_items_from_cart(calculate_cart, count_items_in_cart) {
+    let click_to_trash = document.querySelectorAll(".cart_trash");
+
+    for(var i = 0; i < click_to_trash.length; i++) {
+            click_to_trash[i].addEventListener("click", del_function, false);
+    }
+
+    function del_function(evt) {
+            evt.preventDefault();
+            this.parentNode.parentNode.parentNode.parentNode.remove();
+
+            calculate_cart();
+            count_items_in_cart();
+    }
+};
+
+module.exports = funct_delete_items_from_cart;
--- script/script.js
+++ script/script.js
@@ -5,10 +5,11 @@ import $ from "jquery";
     const search_dropdown = require("./modules/search_dropdown");
     get_product_to_cart = require("./module/get_product_to_cart.js"),
     add_to_card = require("./module/add_to_card.js");
+    delete_items_from_cart = require("./module/delete_from_cart.js"),
 
     get_product_to_cart();
     search_dropdown();
-    add_to_card();
+    add_to_card(delete_items_from_cart);
 
 
 
